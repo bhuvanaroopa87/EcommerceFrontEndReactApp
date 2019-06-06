55d9fbe5948560e0ddbcb77630d1fd2de46b0cf1
bhuvanaroopa87 <bhuvana.roopa87@gmail.com>
2019-06-06 17:52:47 +0200

    products added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./components/navBar";
+import Products from "./components/products";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 2 },
+      { id: 3, value: 4 },
+      { id: 4, value: 6 },
+      { id: 5, value: 8 }
+    ]
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDelete = id => {
+    console.log("handle delete in countersssssssss.jsx :", id);
+    const filteredCounters = this.state.counters.filter(c => c.id !== id);
+    this.setState({ counters: filteredCounters });
+  };
+
+  handleReset = () => {
+    const resetCountersList = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    console.log("handle reset : ", resetCountersList);
+    this.setState({ counters: resetCountersList });
+  };
+
+  getTotalCount() {
+    return this.state.counters.reduce((a, c) => a + c.value, 0);
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar totalCount={this.getTotalCount()} />
+        <main className="container">
+          <Products />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+//Display
+
+//Table of products  (image (not from picsum , ))
+//get products information from  'fakeProductsService'
+
+//Actions
+
+//add to cart , delete from cart , Increment , Decrement
+//Empty Cart
+
+//Twitter comment : How do you feel learning react
+// @reactjs   @JavaScript  @SreenuDoosari
---
+++ src/components/products.jsx
@@ -0,0 +1,132 @@
+import React, { Component } from "react";
+import { getProducts } from "../services/fakeProductService";
+import Like from "./common/like";
+class Products extends Component {
+  state = {
+    products: getProducts()
+  };
+
+  handleDeleteFromProducts = productToDelete => {
+    const actualProducts = this.state.products;
+    const filteredProducts = actualProducts.filter(
+      product => product._id !== productToDelete._id
+    );
+    this.setState({ products: filteredProducts });
+  };
+
+  handleAddProductToCart = productToAdd => {
+    const products = [...this.state.products];
+    const index = products.indexOf(productToAdd);
+    products[index].numOfItemsInCart++;
+    this.setState({ products });
+  };
+
+  handleDeleteProductFromCart = productToDeleteFromCart => {
+    const products = [...this.state.products];
+    const index = products.indexOf(productToDeleteFromCart);
+    products[index].numOfItemsInCart--;
+    this.setState({ products });
+  };
+
+  getTotalNumOfItemsInCart() {
+    return this.state.products.reduce((a, c) => a + c.numOfItemsInCart, 0);
+  }
+
+  showNoProductsWarning(numOfProducts) {
+    if (numOfProducts === 0) {
+      return " No products found";
+    }
+  }
+
+  handleLike(likedProduct) {
+    const products = [...this.state.products];
+    const index = products.indexOf(likedProduct);
+    products[index].liked = !products[index].liked;
+    this.setState({ products });
+  }
+  handleReset = () => {
+    const resetProductLists = this.state.products.map(p => {
+      p.numOfItemsInCart = 0;
+      return p;
+    });
+    this.setState({ products: resetProductLists });
+  };
+  render() {
+    const { length: count } = this.state.products;
+
+    return (
+      <React.Fragment>
+        <button className="btn btn-primary pull-right">
+          <i className="fa fa-shopping-cart" aria-hidden="true" />
+          <span className="badge badge-light m-2">
+            {this.getTotalNumOfItemsInCart()}
+          </span>
+        </button>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Image</th>
+              <th>Name</th>
+              <th>
+                Price <i className="fa fa-eur" aria-hidden="true" />
+              </th>
+              <th>Category</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.showNoProductsWarning(count)}
+            {this.state.products.map(product => (
+              <tr key={product._id}>
+                <td>
+                  <img
+                    src={product.image}
+                    width={50}
+                    height={50}
+                    alt={product.name}
+                  />
+                </td>
+                <td>{product.name}</td>
+                <td>{product.price}</td>
+                <td>{product.category.name}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleAddProductToCart(product)}
+                    className="btn btn-success"
+                  >
+                    +
+                  </button>
+                  <button
+                    onClick={() => this.handleDeleteProductFromCart(product)}
+                    className="btn btn-warning m-2"
+                    disabled={product.numOfItemsInCart === 0 ? "disabled" : ""}
+                  >
+                    -
+                  </button>
+                  <Like
+                    liked={product.liked}
+                    onClick={() => this.handleLike(product)}
+                  />
+                  <button
+                    onClick={() => this.handleDeleteFromProducts(product)}
+                    className="btn btn-danger m-2"
+                  >
+                    x
+                  </button>
+                  <button
+                    onClick={() => this.handleReset()}
+                    className="btn btn-primary m-2"
+                  >
+                    Reset
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Products;
